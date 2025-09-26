43c0b6908b90676b5df718379bfa3f2c31082cd2
Bence Horváth <66383088+Benc313@users.noreply.github.com>
2025-09-27 00:58:48 +0200

    feat: Add RaceRequest and RaceResponse classes for race management

---
+++ ThesisBackend/ThesisBackend.Domain/Messages/RaceResponse.cs
@@ -0,0 +1,36 @@
+﻿using ThesisBackend.Domain.Models;
+
+namespace ThesisBackend.Domain.Messages;
+
+public class RaceResponse
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int CreatorId { get; set; }
+    public int? CrewId { get; set; }
+    public RaceType RaceType { get; set; }
+    public string Location { get; set; }
+    public string Coordinates { get; set; }
+    public bool Private { get; set; }
+    public DateTime Date { get; set; }
+    public List<UserResponse> Users { get; set; } = new();
+    
+    public RaceResponse(){}
+    
+    public RaceResponse(Race race)
+    {
+        Id = race.Id;
+        Name = race.Name;
+        Description = race.Description;
+        CreatorId = race.CreatorId;
+        CrewId = race.CrewId;
+        RaceType = race.RaceType;
+        Location = race.Location;
+        Coordinates = race.Coordinates;
+        Private = race.Private;
+        Date = race.Date;
+        Users = race.Users.Select(u => new UserResponse(u)).ToList();
+    }
+
+}
