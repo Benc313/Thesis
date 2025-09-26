fcb0b4dbe42c1f2500b6af1c97cb9e84fc57d859
Bence Horváth <66383088+Benc313@users.noreply.github.com>
2025-09-26 22:26:00 +0200

    feat: Add CrewRequest, CrewResponse, and UserCrewRequest classes for crew management

---
+++ ThesisBackend/ThesisBackend.Domain/Messages/CrewResponse.cs
@@ -0,0 +1,20 @@
+﻿using ThesisBackend.Domain.Models;
+
+namespace ThesisBackend.Domain.Messages;
+
+public class CrewResponse
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string ImageLocation { get; set; }
+    public List<UserResponse> Users { get; set; } = new();
+
+    public CrewResponse(Crew crew)
+    {
+        Id = crew.Id;
+        Name = crew.Name;
+        Description = crew.Description;
+        ImageLocation = crew.ImageLocation;
+    }
+}
